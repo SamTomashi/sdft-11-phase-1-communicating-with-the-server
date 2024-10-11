b0e99b33fef1f1d323f06a654a73dfbcceab0f11
Sam Tomashi <samtomashikayembi@yahoo.fr>
2024-10-11 16:46:58 +0300

    view student details in a table

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 /*** Business Logic */
 
+// Fetch all students from the database
 function getStudents() {
-  fetch("http://localhost:3000/students",{
+  fetch("http://localhost:3003/students",{
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -9,11 +10,26 @@ function getStudents() {
     },
   })
     .then((response) => response.json())
-    .then((students) => displayStudents(students));
+    .then((students) => displayStudents(students));// pass fetched data to the UI logic
+}
+
+//fetching student by id from the database
+function getStudentById(id){
+  return fetch(`http://localhost:3003/students/${id}`,{
+    method: "GET", // if you use POST, remember to pass the data to the body commented below
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json"
+    },
+    // body: JSON.stringify({})
+  })
+    .then((response) => response.json())
+    .then((data) => data);
 }
 
 /*** UI Logic */
 function displayStudents(students) {
+  const cardsContainer = document.querySelector("#students-container")
   const cards = students.map((student) => {
     return `
     <div class="card">
@@ -26,13 +42,39 @@ function displayStudents(students) {
           <h6 class="card-text">
             ${student.name}
           </h6>
-          <button class="btn btn-primary btn-sm">View Student</button>
+          <button id=${student.id} class="btn-toggle btn btn-primary btn-sm">View</button>
+          <ul class="hidden">
+            <li>${student.gender}</li>
+            <li>${student.age}</li>
+            <li>${student.cohort}</li>
+          </ul>
         </div>
       </div>
     `;
   });
 
-  document.querySelector("#students-container").innerHTML += cards;
+  cardsContainer.innerHTML += cards;
+
+  cardsContainer.addEventListener('click', function(event){
+    if(event.target.classList.contains("btn-toggle")){
+      // event.target.nextElementSibling.classList.toggle("hidden");
+      getStudentById(event.target.id)
+      .then((response) => viewStudentDetails(response))
+    }
+  })
+}
+
+function viewStudentDetails(student){
+  document.querySelector("#current-student").innerHTML = `
+  <tr>
+    <td>${student.id}</td>
+        <td>${student.name}</td>
+        <td>${student.gender}</td>
+        <td>${student.age}</td>
+        <td>${student.cohort}</td>
+  </tr>
+  `
+  
 }
 window.addEventListener("DOMContentLoaded", function () {
   getStudents();
